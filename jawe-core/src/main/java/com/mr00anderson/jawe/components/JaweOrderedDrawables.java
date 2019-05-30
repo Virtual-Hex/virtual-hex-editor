0a571a8246a45e55ac9c257ccdfe49c1d7c8e7c8
mr00anderson <daniel.java.anderson@gmail.com>
2019-05-30 12:25:07 -0400

    - Updated to have queues to prevent concurrent modification error, must use these to add or remove windows from a drawable

--- jawe-core/src/main/java/com/mr00anderson/jawe/components/JaweOrderedDrawables.java
+++ jawe-core/src/main/java/com/mr00anderson/jawe/components/JaweOrderedDrawables.java
@@ -2,14 +2,18 @@ package com.mr00anderson.jawe.components;
 
 import com.artemis.PooledComponent;
 import com.mr00anderson.jawe.drawables.JaweDrawable;
+import com.mr00anderson.jawe.drawables.JaweWindow;
 import org.ice1000.jimgui.JImGui;
 
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class JaweOrderedDrawables extends PooledComponent implements JaweDrawable {
 
     public LinkedList<JaweDrawable> drawables = new LinkedList<>();
+    public ConcurrentLinkedQueue<JaweWindow> addWindowQueue = new ConcurrentLinkedQueue<>();
+    public ConcurrentLinkedQueue<JaweWindow> removeWindowQueue = new ConcurrentLinkedQueue<>();
 
     public JaweOrderedDrawables() {
     }
@@ -25,6 +29,16 @@ public class JaweOrderedDrawables extends PooledComponent implements JaweDrawabl
 
     @Override
     public void draw(JImGui imGui) {
+        JaweWindow addPoll = addWindowQueue.poll();
+        if(addPoll != null){
+            drawables.add(addPoll);
+        }
+
         drawables.forEach((d) -> d.draw(imGui));
+
+        JaweWindow removePoll = removeWindowQueue.poll();
+        if(removePoll != null){
+            drawables.remove(removePoll);
+        }
     }
 }
